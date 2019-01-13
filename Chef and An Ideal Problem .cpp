eb7577e0f6a447daac0a9710de664c28b3ecb65b
Md. Zahid Fesabelilla <33966829+fesabelilla@users.noreply.github.com>
2019-01-13 10:32:26 +0600

    Add files via upload

---
+++ Chef and An Ideal Problem .cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    cout<<a<<endl;
+    int b;
+    cin>>b;
+    if(b==1)
+        cout<<2;
+    else if(b==2)
+        cout<<1;
+
+
+    return 0;
+}
